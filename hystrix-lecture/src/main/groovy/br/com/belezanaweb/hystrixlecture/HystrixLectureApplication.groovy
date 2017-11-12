c77eafa511ce3143baedb94785c49ac1fc0da188
Bruno Silva <brunogs.sp@gmail.com>
2017-11-11 23:33:32 -0200

    add: hystrix lecture required dependencies'

---
+++ hystrix-lecture/src/main/groovy/br/com/belezanaweb/hystrixlecture/HystrixLectureApplication.groovy
@@ -0,0 +1,12 @@
+package br.com.belezanaweb.hystrixlecture
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class HystrixLectureApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run HystrixLectureApplication, args
+	}
+}
